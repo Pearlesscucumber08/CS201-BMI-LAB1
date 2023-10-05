a83e2fba870c4b64554b4d3154e65c2d8310f6ff
Ricardo Haduca <ibrigitteaeyahoo@gmail.com>
2023-10-05 13:26:44 +0800

    feat: added javascript into my html

---
+++ script.js
@@ -0,0 +1,30 @@
+//ACT 1
+//1 + 2
+// 1 - 2
+//1 * 2
+// 1/2
+//check 1 mod 2 
+//check the power of 1
+let firstName = " Brigitte Mae";
+let last_name = " Ibeng";
+let fullName = firstName + ' ' + last_name;
+console.log (fullName);
+console.log("BSCS-2A");
+console.log("CS201-BMI-LAB1");
+
+let firstFavNum = 9;
+let secondFavNum = 8;
+let add = firstFavNum + secondFavNum;
+let sub = firstFavNum - secondFavNum;
+let mul = firstFavNum * secondFavNum;
+let div = firstFavNum / secondFavNum;
+let mod = firstFavNum % secondFavNum;
+//var power = Math.pow(firstFavNum, secondFavNum);
+var power = firstFavNum ** secondFavNum;
+console.log (firstFavNum +  " + " + secondFavNum + " = " + add);
+console.log ( firstFavNum + " - " + secondFavNum + " = " + sub);
+console.log ( firstFavNum + " * "+ secondFavNum + " = " + mul);
+console.log ( firstFavNum + " / " + secondFavNum + " = " + div);
+console.log (mod);
+console.log (power);
+
